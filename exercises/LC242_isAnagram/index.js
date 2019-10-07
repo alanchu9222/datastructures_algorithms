cff4f05f24c6b9508ee3bc6f6037ee5c3d67ed0e
Alan Chu <alan_chu@yahoo.com>
2019-10-07 11:00:02 +1100

    LC242 solved

--- exercises/LC242_isAnagram/index.js
+++ exercises/LC242_isAnagram/index.js
@@ -1,3 +1,14 @@
-function isAnagram(s, t) {}
+function isAnagram(s, t) {
+  const aa = s
+    .split("")
+    .sort()
+    .join();
+  const bb = t
+    .split("")
+    .sort()
+    .join();
+
+  return aa === bb;
+}
 
 module.exports = isAnagram;
---
+++ testcode/lc242_optimised.js
@@ -0,0 +1,17 @@
+// Given a set of strings -organise them into an array
+// of arrays : each array containing the strings of the same anagram
+const isAnagram = (a, b) => {
+  const aa = a
+    .split("")
+    .sort()
+    .join();
+  const bb = b
+    .split("")
+    .sort()
+    .join();
+
+  console.log(aa);
+  console.log(bb);
+  return aa === bb;
+};
+console.log(isAnagram("abcdef", "cbafed"));
