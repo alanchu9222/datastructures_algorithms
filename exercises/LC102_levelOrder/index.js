66e6a7faa12bab503b321450eb7269afdb13065a
Alan Chu <alan_chu@yahoo.com>
2019-10-09 15:15:53 +1100

    LC104

--- exercises/LC102_levelOrder/index.js
+++ exercises/LC102_levelOrder/index.js
@@ -1,3 +1,19 @@
-function levelOrder(root) {}
+function levelOrder(root) {
+  let result = [];
 
+  getResult = (node, depth) => {
+    if (!node) {
+      return;
+    }
+    if (!result[depth]) {
+      result[depth] = [];
+    }
+    result[depth].push(node.val);
+    getResult(node.left, depth + 1);
+    getResult(node.right, depth + 1);
+  };
+
+  getResult(root, 0);
+  return result;
+}
 module.exports = levelOrder;
--- exercises/LC104_maxDepth/index.js
+++ exercises/LC104_maxDepth/index.js
@@ -1,3 +1,17 @@
-function maxDepth(root) {}
+function maxDepth(root) {
+  let maxDepth = 0;
+
+  helper = (node, depth) => {
+    if (!node) {
+      return;
+    }
+    maxDepth = Math.max(depth + 1, maxDepth);
+    helper(node.left, depth + 1);
+    helper(node.right, depth + 1);
+  };
+
+  helper(root, 0);
+  return maxDepth;
+}
 
 module.exports = maxDepth;
