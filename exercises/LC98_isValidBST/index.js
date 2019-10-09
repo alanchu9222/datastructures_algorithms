06bcc6617a7cb1cb8adc79aa9738a50b3fc4a55f
Alan Chu <alan_chu@yahoo.com>
2019-10-09 14:33:09 +1100

    lc98

--- exercises/LC98_isValidBST/index.js
+++ exercises/LC98_isValidBST/index.js
@@ -1,3 +1,34 @@
-function isValidBST(root) {}
+/*
+BST is made of nodes
+Each node = {val, left, right}
+Value = number: Left, Right = node
+When Left or RIght === null, thats the termination of that branch in the tree
 
+*/
+
+function isValidBST(root) {
+  let valid = true;
+
+  checkValid = (node, min, max) => {
+    if (!node) {
+      return;
+    }
+    console.log("node " + node.val + " min " + min + " max " + max);
+
+    if (
+      (min !== null && node.val <= min) ||
+      (max !== null && node.val >= max)
+    ) {
+      valid = false;
+      return;
+    }
+    // The current value  = max for the left node
+    // The current value  = min for the right node
+    // Recursively check that this rule is followed
+    checkValid(node.left, min, node.val);
+    checkValid(node.right, node.val, max);
+  };
+  checkValid(root, null, null);
+  return valid;
+}
 module.exports = isValidBST;
---
+++ testcode/lc98_optimized.js
@@ -0,0 +1,9 @@
+/*
+
+
+This solution requires the test harness with explicit implementation of a Binary Search Tree
+data structure built on Nodes
+
+Each Node contains { val, left, right }
+Refer solution on the test harness
+*/
