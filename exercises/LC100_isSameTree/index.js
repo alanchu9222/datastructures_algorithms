c9723ba36397fcfb70674715369da8b955b533f1
Alan Chu <alan_chu@yahoo.com>
2019-10-09 14:52:34 +1100

    lc100

--- exercises/LC100_isSameTree/index.js
+++ exercises/LC100_isSameTree/index.js
@@ -1,3 +1,27 @@
-function isSameTree(p, q) {}
+function isSameTree(p, q) {
+  let isSameTree = true;
+
+  checkSameTree = (p, q) => {
+    if (p === null && q === null) {
+      return;
+    } else {
+      if (p === null || q === null) {
+        isSameTree = false;
+        return;
+      } else {
+        if (p.val !== q.val) {
+          isSameTree = false;
+          return;
+        }
+      }
+    }
+
+    checkSameTree(p.left, q.left);
+    checkSameTree(p.right, q.right);
+  };
+
+  checkSameTree(p, q);
+  return isSameTree;
+}
 
 module.exports = isSameTree;
