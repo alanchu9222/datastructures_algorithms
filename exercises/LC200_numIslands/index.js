7c8552e197406d4c6d54eea354a0d59f1244e5ba
Alan Chu <alan_chu@yahoo.com>
2019-10-09 15:40:36 +1100

    lc200

--- exercises/LC200_numIslands/index.js
+++ exercises/LC200_numIslands/index.js
@@ -1,3 +1,30 @@
-function numIslands(grid) {}
+function numIslands(grid) {
+  let count = 0;
+  sinkIsland = (grid, row, col) => {
+    if (
+      row < 0 ||
+      col < 0 ||
+      row >= grid.length ||
+      col >= grid[0].length ||
+      grid[row][col] === "0"
+    ) {
+      return;
+    }
+    grid[row][col] = "0";
+    sinkIsland(grid, row - 1, col);
+    sinkIsland(grid, row + 1, col);
+    sinkIsland(grid, row, col - 1);
+    sinkIsland(grid, row, col + 1);
+  };
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] === "1") {
+        count++;
+        sinkIsland(grid, row, col);
+      }
+    }
+  }
+  return count;
+}
 
 module.exports = numIslands;
