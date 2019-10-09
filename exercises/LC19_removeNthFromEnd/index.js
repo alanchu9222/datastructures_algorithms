9d77030b71c662830fa8926ceb9e0a426f2e7bbb
Alan Chu <alan_chu@yahoo.com>
2019-10-09 17:09:12 +1100

    lc19

--- exercises/LC19_removeNthFromEnd/index.js
+++ exercises/LC19_removeNthFromEnd/index.js
@@ -1,3 +1,21 @@
-function removeNthFromEnd(head, n) {}
+function removeNthFromEnd(head, n) {
+  let slowNode = head;
+  let fastNode = head;
+  for (let i = 0; i < n; i++) {
+    fastNode = fastNode.next;
+  }
+  if (!slowNode.next) {
+    // edge case - if only has one element, then that element must be removed
+    return null;
+  }
+
+  while (fastNode.next) {
+    fastNode = fastNode.next;
+    slowNode = slowNode.next;
+  }
+
+  slowNode.next = slowNode.next.next;
+  return head;
+}
 
 module.exports = removeNthFromEnd;
