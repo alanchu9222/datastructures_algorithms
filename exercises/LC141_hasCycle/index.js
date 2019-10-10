9c6052d0f2788e0329012c71ef06d0a12d1c2571
Alan Chu <alan_chu@yahoo.com>
2019-10-10 14:29:26 +1100

    lc206

--- exercises/LC141_hasCycle/index.js
+++ exercises/LC141_hasCycle/index.js
@@ -1,3 +1,15 @@
-function hasCycle(head) {}
+function hasCycle(head) {
+  let fast = head;
+  let slow = head;
+
+  while (fast.next && fast.next.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow.val === fast.val) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = hasCycle;
--- exercises/LC206_reverseList/index.js
+++ exercises/LC206_reverseList/index.js
@@ -1,3 +1,14 @@
-const reverseList = head => {};
+const reverseList = head => {
+  let prev = null;
+  let current = head;
+
+  while (current) {
+    let temp = current.next;
+    current.next = prev;
+    prev = current;
+    current = temp;
+  }
+  return prev;
+};
 
 module.exports = reverseList;
